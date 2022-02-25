41dc3ddfde4f2fa31604f234026772233eb89aff
jakbin <jakbin4747@gmail.com>
2022-02-25 20:55:54 +0530

    nodejs scripts added

---
+++ README.md
@@ -0,0 +1,7 @@
+# deb-scripts
+
+Collections of bash scripts making deb files
+
+## Available scripts 
+
+1. nodejs v16.14 
---
+++ nodejs/nodeInstall.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+wget 'https://nodejs.org/dist/v16.14.0/node-v16.14.0-linux-x64.tar.xz'
+
+tar -xvf node-v16.14.0-linux-x64.tar.xz
+
+mkdir -p node-v16/usr
+
+cp -r node-v16.14.0-linux-x64/* node-v16/usr
+
+wget 'https://raw.githubusercontent.com/jakbin/deb-scripts/main/nodejs/control'
+
+mkdir node-v16/DEBIAN
+
+cp control node-v16/DEBIAN/
+
+dpkg -b node-v16
