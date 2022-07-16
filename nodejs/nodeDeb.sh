224924255784e744a6a8832a5128641592a0b18f
Dhruv Jagdish <65591973+jakbin@users.noreply.github.com>
2022-07-16 09:23:10 +0530

    Update nodeDeb.sh

--- nodejs/nodeDeb.sh
+++ nodejs/nodeDeb.sh
@@ -2,7 +2,7 @@
 
 # repo url:- https://github.com/jakbin/deb-scripts
 
-version='v16.15.0'
+version='v16.16.0'
 if [[ -f 'node-'$version'-linux-x64.tar.xz' ]]; then
 	echo "file node-$version-linux-x64.tar.xz already exits"
 else
