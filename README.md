f60bfddb18c712b567cbb7d54f9c88ed9ccc564a
jakbin <jakbin4747@gmail.com>
2023-07-02 21:03:32 +0530

    update nodejs version

--- README.md
+++ README.md
@@ -4,5 +4,5 @@ Collections of bash scripts making deb files
 
 ## Available scripts 
 
-1. nodejs : v18.16.0
+1. nodejs : v18.16.1
 2. telegram : v4.6.5
--- nodejs/nodeDeb.sh
+++ nodejs/nodeDeb.sh
@@ -3,7 +3,7 @@
 # repo url:- https://github.com/jakbin/deb-scripts
 
 
-version='v18.16.0'
+version='v18.16.1'
 
 if [ -f 'node-'$version'-linux-x64.tar.xz' ]; then
 	echo "file node-$version-linux-x64.tar.xz already exits"
