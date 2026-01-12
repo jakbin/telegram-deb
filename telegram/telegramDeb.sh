af526809afa2c2da24de879fcc9541e5e34138a4
jakbin <jakbin4747@gmail.com>
2026-01-12 13:58:01 +0530

    update version to 6.4.2 and fix file path references in telegramDeb.sh

--- telegram/telegramDeb.sh
+++ telegram/telegramDeb.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-version='v5.13.1'
+version='6.4.2'
 
 if [ -f 'tsetup.'$version'.tar.xz' ]; then
 	echo "file tsetup.$version.tar.xz already exits"
@@ -13,21 +13,21 @@ mkdir -p telegram-$version/usr/bin
 cp -r Telegram/Telegram telegram-$version/usr/bin
 mv telegram-$version/usr/bin/Telegram telegram-$version/usr/bin/telegram
 
-if [ -f 'share.zip' ]; then
+if [ -f 'telegram/share.zip' ]; then
 	echo "share.zip file already exits"
 else
 	wget 'https://raw.githubusercontent.com/jakbin/telegram-deb/main/telegram/share.zip'
 fi
 
 unzip share.zip
-cp -R share telegram-$version/usr/
+cp -R telegram/share telegram-$version/usr/
 
-if [ -f 'control' ]; then
+if [ -f 'telegram/control' ]; then
 	echo "control file already exits"
 else
 	wget 'https://raw.githubusercontent.com/jakbin/telegram-deb/main/telegram/control'
 fi
 
 mkdir telegram-$version/DEBIAN
-cp control telegram-$version/DEBIAN/
+cp telegram/control telegram-$version/DEBIAN/
 dpkg -b telegram-$version
